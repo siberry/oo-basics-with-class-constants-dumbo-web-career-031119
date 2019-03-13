e3c740b1da5a402c14e25c1213bf809f3b94e461
Serena Berry <siberry226@gmail.com>
2019-03-13 21:01:40 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -12,8 +12,9 @@ class Book
     puts "Flipping the page...wow, you read fast!"
   end
   
-  def genre=(new_genre)
-    GENRES << @genre
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre
   end
   
 end
